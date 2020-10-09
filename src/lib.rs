59307d0aea5bc21209bf210c5b31489f0eb571ac
Piet Geursen <pietgeursen@gmail.com>
2020-10-09 19:56:36 +1300

    Format

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@
 //! Spec defined [here](https://github.com/ssbc/envelope-spec/blob/master/encoding/slp.md)
 //!
 
-use bytes::{BufMut, Buf};
+use bytes::{Buf, BufMut};
 use snafu::{ResultExt, Snafu};
 use std::convert::TryInto;
 use std::io::Read;
@@ -22,11 +22,12 @@ pub enum Error {
 #[derive(PartialEq, Debug)]
 pub struct SLP(pub Vec<Vec<u8>>);
 
+/// A type that owns a collection of bytes that can be encoded using the SLP encoding.
 impl SLP {
     pub fn encode_write<W: Write>(&self, writer: &mut W) -> Result<(), Error> {
         self.0.iter().try_for_each(|bytes| {
             let byte_len: u16 = bytes.len().try_into().context(ItemTooLong)?;
-            let mut length_buf = [0u8;2];
+            let mut length_buf = [0u8; 2];
             (&mut length_buf[..]).put_u16_le(byte_len);
 
             writer.write(&length_buf).context(WriteError)?;
@@ -38,12 +39,12 @@ impl SLP {
         Ok(())
     }
 
-    pub fn decode_read<R: Read>(reader: &mut R) -> Result<Self, Error>{
+    pub fn decode_read<R: Read>(reader: &mut R) -> Result<Self, Error> {
         let mut items = Vec::new();
 
-        let mut length_buf = [0u8;2];
+        let mut length_buf = [0u8; 2];
 
-        while let Ok(_) = reader.read_exact(&mut length_buf){
+        while let Ok(_) = reader.read_exact(&mut length_buf) {
             let byte_len = (&length_buf[..]).get_u16_le();
             let mut item = Vec::with_capacity(byte_len as usize);
             item.resize(byte_len as usize, 0);
@@ -53,7 +54,7 @@ impl SLP {
         Ok(SLP(items))
     }
 
-    pub fn into_inner(self) -> Vec<Vec<u8>>{
+    pub fn into_inner(self) -> Vec<Vec<u8>> {
         self.0
     }
 }
@@ -63,12 +64,11 @@ mod tests {
     use crate::*;
     #[test]
     fn encode_decode() {
-        let data = vec![vec![1u8,2,3], vec![4u8, 5, 6, 7]];
+        let data = vec![vec![1u8, 2, 3], vec![4u8, 5, 6, 7]];
         let slp = SLP(data);
 
         let mut encode_writer = vec![];
         slp.encode_write(&mut encode_writer).unwrap();
-        
 
         let decoded = SLP::decode_read(&mut encode_writer.as_slice()).unwrap();
 
